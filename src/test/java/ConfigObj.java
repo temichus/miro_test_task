3c49652e3cba7fcb36ecd91683ca2a79de6e50ad
artsiom <temichus@gmail.com>
2020-10-10 18:35:30 +0300

    first commit

---
+++ src/test/java/ConfigObj.java
@@ -0,0 +1,9 @@
+// Objekt for parsing test config
+public class ConfigObj {
+    public String url;
+    public String user1;
+    public String password1;
+    public String user2;
+    public String password2;
+
+}
